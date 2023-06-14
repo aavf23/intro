5c854c59c08eabd94c49f507b19f62fbd04bdd4a
aavf23 <136551765+aavf23@users.noreply.github.com>
2023-06-14 05:17:57 +0100

    Update maiorDeVarios.py
    Ou usamos  m ou maior

--- maiorDeVarios.py
+++ maiorDeVarios.py
@@ -4,20 +4,20 @@ def main():
     print('O maior é:', pede_numero(maior))
 
 
-def pede_numero(m):
+def pede_numero(maior):
     while True:
         num = int(input('Qual o número? '))
         if num < 0:
             break
         else:
-            valida_maior(m, num)
-    return m
+            valida_maior(maior, num)
+    return maior
 
 
-def valida_maior(m, num):
-    if num > m:
-        m = num
-    return m
+def valida_maior(maior, num):
+    if num > maior:
+        maior = num
+    return maior
 
 
 main()
